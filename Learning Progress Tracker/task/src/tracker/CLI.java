5b06a94d04fce67a012c0e76a421075d0070aa97
ahmedhosnypro <ahmedhosnypro@gmail.com>
2022-05-25 05:24:24 +0200

    No empty lines here

---
+++ Learning Progress Tracker/task/src/tracker/CLI.java
@@ -0,0 +1,43 @@
+package tracker;
+
+public class CLI {
+    private CLI() {
+    }
+
+    static void run() {
+        while (true) {
+            listenToUserCommand();
+        }
+    }
+
+    private static void listenToUserCommand() {
+        String commandInput = Main.scanner.nextLine().trim().toUpperCase();
+        if (commandInput.equals("") || commandInput.matches("\\s+")) {
+            System.out.println("No input.");
+        } else if (isKnownCommand(commandInput)) {
+            runCommand(Command.valueOf(commandInput));
+        } else {
+            System.out.println("Error: unknown command!");
+        }
+    }
+
+    private static void runCommand(Command command) {
+        switch (command) {
+            case EXIT -> exit();
+        }
+    }
+
+    private static void exit() {
+        System.out.println("Bye!");
+        System.exit(0);
+    }
+
+    private static boolean isKnownCommand(String commandInput) {
+        try {
+            Command.valueOf(commandInput);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ Learning Progress Tracker/task/src/tracker/Main.java
@@ -0,0 +1,12 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Learning Progress Tracker");
+        CLI.run();
+    }
+}
